73661fb6fcb800109b1690c22be2def901a5399f
random-development-hub <github.dev.rand@gmail.com>
2021-10-05 15:08:17 +0300

    tried something, failed, added new little feature as compensation

--- index.js
+++ index.js
@@ -4,76 +4,22 @@ var playing = [];
 var loop = false;
 
 const prefix = "bg";
+const default_volume = 0.20;
 const commands = ["pause", "play", "src", "loop", "pause", "vol"];
 
 function p(s) { return prefix + s }
 
-function n(s, t = 3e3) {
+function n(s) {
     let random = prefix + "-" + (Math.random() + 1).toString(36).substring(7);
     powercord.api.notices.sendToast(random, {
         header: "background-player",
         content: s,
-        timeout: t,
+        timeout: 15e2,
     });
 }
 
 module.exports = class BackgroundPlayer extends Plugin {
     async startPlugin() {
-        /*
-         * wip
-         */
-
-        // powercord.api.commands.registerCommand({
-        //     command: p("queue"),
-        //     description: "Queue songs",
-        //     usage: "{c} [url/s]",
-        //     executor: (args) => {
-        //         if (args[0]) {
-        //             for (var i = 0; i < args.length; i++) {
-        //                 playing.push(args[0]);
-        //                 var filename = args[0].split("/").pop().split("#")[0].split("?")[0];
-        //                 n("Added to queue: " + filename, 15e2);
-        //             }
-        //         } else {
-        //             n("No URLs provided");
-        //         }
-        //     }
-        // });
-
-        // powercord.api.commands.registerCommand({
-        //     command: p("playqueue"),
-        //     description: "Play the queue",
-        //     usage: "{c}",
-        //     executor: () => {
-        //         if (playing.length > 0) {
-        //             for (var i = 0; i < playing.length; i++) {
-        //                 playing.shift();
-        //                 playing[i].play();
-
-        //                 setTimeout(() => {
-        //                     this.play(playing[i + 1]);
-        //                 }, playing[i].duration * 1e3);
-        //             }
-        //         } else {
-        //             n("Queue is empty");
-        //         }
-        //     }
-        // });
-
-        // powercord.api.commands.registerCommand({
-        //     command: p("pausequeue"),
-        //     description: "Pause the queue",
-        //     usage: "{c}",
-        //     executor: () => {
-        //         if (playing.length > 0) {
-        //             playing.shift();
-        //             playing.pause();
-        //         } else {
-        //             n("Queue is empty");
-        //         }
-        //     }
-        // });
-
         powercord.api.commands.registerCommand({
             command: p("play"),
             description: "Play file in background by link.",
@@ -81,7 +27,7 @@ module.exports = class BackgroundPlayer extends Plugin {
             executor: (args) => {
                 for (var i = 0; i < args.length; i++) {
                     var audio = new Audio(args[0]);
-                    audio.volume = 0.3;
+                    audio.volume = default_volume;
 
                     if (loop) {
                         audio.loop = true;
@@ -89,13 +35,11 @@ module.exports = class BackgroundPlayer extends Plugin {
 
                     playing = audio;
                     playing.play();
-
                     var filename = playing.src.split("/").pop().split("#")[0].split("?")[0];
                     n("Playing " + filename + " at volume " + audio.volume);
                 }
             }
         });
-
         powercord.api.commands.registerCommand({
             command: p("pause"),
             description: "Pause current file playing.",
@@ -105,7 +49,6 @@ module.exports = class BackgroundPlayer extends Plugin {
                 n(playing.paused ? "Paused file." : "Resumed file.")
             }
         });
-
         powercord.api.commands.registerCommand({
             command: p("loop"),
             description: "Loop current file playing.",
@@ -114,11 +57,9 @@ module.exports = class BackgroundPlayer extends Plugin {
                 var oldplaying = playing;
                 loop = !playing.loop
                 playing.loop = loop;
-
                 n(oldplaying.loop ? "Looping file." : "Stopped looping file.");
             }
         });
-
         powercord.api.commands.registerCommand({
             command: p("src"),
             description: "Print current source link",
@@ -130,7 +71,6 @@ module.exports = class BackgroundPlayer extends Plugin {
                 }
             }
         });
-
         powercord.api.commands.registerCommand({
             command: p("vol"),
             description: "Set volume of current file playing.",
@@ -139,12 +79,14 @@ module.exports = class BackgroundPlayer extends Plugin {
                 if (Number(args[0])) {
                     playing.volume = args[0];
                     n("Set volume to " + playing.volume);
+                } else if (args[0] == "default") {
+                    playing.volume = default_volume;
+                    n("Set volume to default. (" + default_volume + ")");
                 } else {
                     n("Volume: " + playing.volume);
                 }
             }
         });
-
         powercord.api.commands.registerCommand({
             command: p("elapsed"),
             description: "Print elapsed time of current file playing.",
@@ -153,8 +95,25 @@ module.exports = class BackgroundPlayer extends Plugin {
                 n("Elapsed time: " + Math.floor(playing.currentTime) + "s");
             }
         });
+        powercord.api.commands.registerCommand({
+            command: p("duration"),
+            description: "Print duration of current file playing.",
+            usage: "{c}",
+            executor: (args) => {
+                n("Duration: " + Math.floor(playing.duration) + "s");
+            }
+        });
+        powercord.api.commands.registerCommand({
+            command: p("replay"),
+            description: "Replays current file playing.",
+            usage: "{c}",
+            executor: (args) => {
+                playing.currentTime = 0;
+                playing.pause();
+                n("Replayed file.");
+            }
+        });
     }
-
     pluginWillUnload() {
         for (var i = 0; i < commands.length; i++) {
             powercord.api.commands.unregisterCommand(commands[i]);
